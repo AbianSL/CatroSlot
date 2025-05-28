5e6d08dcf8d3052bc3e6dde76417c7558b0f4956
AbianSL <abisanle@gmail.com>
2025-05-28 17:47:59 +0100

    img_gen [add]: add function to be called by the main

---
+++ src/img_gen.py
@@ -0,0 +1,14 @@
+def generate_images(talent: str, class_name: str, color: str, version: bool, file_path: str) -> None:
+    """
+    Generates images based on the provided parameters.
+    :param talent: The talent of the card (e.g., "shadow", "draconic").
+    :param class_name: The class of the card (e.g., "warrior", "illusionist").
+    :param color: The color of the card ("red", "yellow", "blue").
+    :param version: The version of the card [New (True) or Old (False)].
+    """
+    if version:
+        # TODO: Implement version-specific image generation logic
+        pass
+    else:
+        # TODO: Implement default image generation logic
+        pass
