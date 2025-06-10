8c34edd7628fa001fcf06193888f675490484f9b
AbianSL <abisanle@gmail.com>
2025-06-10 22:05:10 +0100

    main.py [add]: add main lines to read all base folder files

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    print("Hello from catroslot!")
+import os
+from src.img_gen import generate_images
 
+def main():
+    base_directory = os.path.dirname(os.path.abspath(__file__)) + "/base"
+    for file_name in os.listdir(base_directory):
+        complete_route = os.path.join(base_directory, file_name)
+        if os.path.isfile(complete_route):
+            print(file_name)
+    
 if __name__ == "__main__":
     main()
