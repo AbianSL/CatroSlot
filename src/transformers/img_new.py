3016a4bcdebab95617ad36b27789d819c2dc1881
AbianSL <abisanle@gmail.com>
2025-09-10 00:44:38 +0100

    img_new [modify]: move resize images to new class

--- src/transformers/img_new.py
+++ src/transformers/img_new.py
@@ -7,14 +7,22 @@ class NewImageTransform(ImageTransform):
         talent: str | None,
         class_name: str,
         action: str,
-    color_base: str,
+        color_base: str,
         image_file: str,
     ) -> None:
         super().__init__(image_file, color_base, action, talent)
         self._defend_position = (392, 554)
         self._power_position = (29, 554)
-        self._cost_position = (30, 30)
+        self._pitch_position = (30, 30)
+        self._cost_position = (371, 30)
+
         self._blue_pitch_position = (57, 53)
         self._yellow_pitch_position = (35, 53)
         self._red_pitch_position = (45, 33)
         self._color_bar_position = (70, 28)
+
+        # resize images
+        self._non_symbol = self._non_symbol.resize((50, 50))
+        self._non_color_bar = self._non_color_bar.resize((312, 6))
+        self._pitch_img = self._pitch_img.resize((17, 17))
+        self._small_non_symbol = self._small_non_symbol.copy().resize((30, 30))
