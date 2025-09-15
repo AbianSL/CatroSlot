c2ed41744a1345026b02fec574310ca374f7c90d
AbianSL <abisanle@gmail.com>
2025-09-15 23:20:30 +0100

    run [modify]: install dependencies if it was not already

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-source .venv/bin/activate
+if [ ! -d ".venv" ]; then
+    uv sync
+    echo -e "\nVirtual environment created and dependencies installed."
+    sleep 2 
+else
+    source .venv/bin/activate
+fi
 
 python3 main.py
